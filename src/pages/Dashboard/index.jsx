2ff9a65a92fb82759030b04679ade1002ea4b3b5
Khadija <i200760@nu.edu.pk>
2023-12-10 01:13:00 +0500

    minor fixes

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Sidebar } from 'react-pro-sidebar';
-import { useNavigate } from 'react-router-dom';
 import { Img, Text, List } from 'components';
+import { useNavigate } from 'react-router-dom';
+import Navigation from 'pages/Sidebar';
 
-// Styles
 const projectContainerStyles = {
   position: 'relative',
   perspective: '1000px',
@@ -11,11 +10,16 @@ const projectContainerStyles = {
 
 const styles = {
   projectItem: {
-    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     width: '100%',
-    height: '100%',
-    backfaceVisibility: 'hidden',
-    transformStyle: 'preserve-3d',
+    minHeight: '100px', // Adjust as needed
+    padding: '16px',
+    backgroundColor: '#FFFFFF',
+    borderRadius: '8px',
+    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+    marginBottom: '16px',
   },
 };
 
@@ -36,134 +40,43 @@ const DashboardPage = () => {
 
   const [projects, setProjects] = useState([]);
   const [tasks, setTasks] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [showCreateButton, setShowCreateButton] = useState(false);
-  const [projectButton, setProjectButton] = useState(false);
+  const [showCreateProjects, setShowCreateProjects] = useState(false);
+  const [showCreateTasks, setShowCreateTasks] = useState(false);
 
   let name = localStorage.getItem("username");
 
   useEffect(() => {
-    // For the sake of this example, using dummy data instead of fetching from an API
     setProjects(dummyProjects);
     setTasks(dummyTasks.map((task) => ({ ...task, hovered: false })));
-
-    setLoading(false);
   }, []);
 
   const handleProjectsHover = () => {
-    setProjectButton(true);
+    setShowCreateProjects(true);
   };
 
   const handleProjectsLeave = () => {
-    if (projectButton) {
-      setProjectButton(false);
-    }
+    setShowCreateProjects(false);
   };
 
   const handleTasksHover = () => {
-    setShowCreateButton(true);
+    setShowCreateTasks(true);
   };
 
   const handleTasksLeave = () => {
-    if (showCreateButton) {
-      setShowCreateButton(false);
-    }
+    setShowCreateTasks(false);
   };
 
-  const handleCreateClick = () => {
+  const handleCreateProjectClick = () => {
     navigate('/newproject');
   };
 
-  const handleTaskClick = () => {
+  const handleCreateTaskClick = () => {
     navigate('/newtask');
   };
 
-  const renderProjects = () => {
-    return (
-      <ul>
-        {projects.map((project) => (
-          <li key={project.id}>
-            <div
-              style={{ ...styles.projectItem }}
-              onMouseEnter={handleProjectsHover}
-              onMouseLeave={handleProjectsLeave}
-            >
-              <Text
-                className="common-pointer"
-                size="txtPoppinsRegular16"
-                color="indigo-800"
-                onClick={() => navigate(`/project/${project.id}`)}
-              >
-                {project.name}
-              </Text>
-              <Text color="indigo-800">{`Due ${project.dueDate}`}</Text>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
-  };
-  
-  const renderTasks = () => {
-    return (
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <div
-              style={{ ...styles.projectItem }}
-              onMouseEnter={handleTasksHover}
-              onMouseLeave={handleTasksLeave}
-            >
-              <Text
-                className="common-pointer"
-                size="txtPoppinsRegular16"
-                color="indigo-800"
-                onClick={() => navigate(`/task/${task.id}`)}
-              >
-                {task.name}
-              </Text>
-              <Text color="indigo-800">{`Due ${task.dueDate}`}</Text>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
-  };
-  
-
   return (
     <div style={{ display: 'flex', flexDirection: 'row', height: '100vh' }}>
-      <div style={{ width: '23%', position: 'relative', backgroundColor: '#EDEFF5' }}>
-        <Sidebar
-          style={{
-            width: '100%',
-            height: '100%',
-            background: 'linear-gradient(180deg, #EDEFF5 0%, white 100%)',
-            cursor: 'pointer',
-          }}
-        >
-          <div className="absolute flex flex-col inset-x-[0] justify-start mx-auto top-[6%] w-[45%]">
-            <Text className="font-bold text-[22px] text-center text-indigo-800 sm:text-lg md:text-xl">
-              ProjectFlow
-            </Text>
-            <Text onClick={() => navigate('/dashboard')} className="ml-7 md:ml-[0] mt-[102px] text-base text-indigo-800 tracking-[0.44px]">
-              Dashboard
-            </Text>
-            <div className="flex flex-col gap-[46px] items-start justify-start md:ml-[0] ml-[35px] mt-[47px]">
-              <Text onClick={() => navigate('/myprojects')} className="md:ml-[0] ml-[3px] text-base text-indigo-800 tracking-[0.44px]">
-                Projects
-              </Text>
-              <Text onClick={() => navigate('/mytasks')} className="text-base text-indigo-800 tracking-[0.44px]">
-                My Tasks
-              </Text>
-              <Text onClick={() => navigate('/apps')} className="md:ml-[0] ml-[7px] text-base text-indigo-800 tracking-[0.44px]">
-                Apps
-              </Text>
-            </div>
-          </div>
-        </Sidebar>
-      </div>
+      <Navigation />
 
       <div style={{ width: '73%', padding: '20px' }}>
         <Text
@@ -192,32 +105,37 @@ const DashboardPage = () => {
             onMouseLeave={handleProjectsLeave}
             className="bg-gray-50 flex flex-1 flex-col items-center justify-start p-8 sm:px-5 rounded-[30px] w-full"
           >
-            <div>
-              <div className="flex flex-col md:gap-10 gap-[81px] justify-start mb-[186px] mt-[23px] w-full">
-                <Text
-                  onClick={handleCreateClick}
-                  style={{
-                    fontSize: projectButton ? '40px' : '22px',
-                    color: '#323F73',
-                    height: '32px',
-                    width: '152px',
-                    cursor: 'pointer',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    margin: projectButton ? '200px 0' : '0',
-                  }}
-                  size="txtPoppinsBold22"
-                >
-                  {projectButton ? 'Create' : 'My Projects'}
-                </Text>
-                <div className="flex flex-col items-start justify-start w-full">
-                  <div className="flex flex-row items-start justify-between ml-1 md:ml-[0] mt-[42px] w-[84%] md:w-full">
-                    {renderProjects()}
+            <Text
+              style={{
+                fontSize: showCreateProjects ? '40px' : '22px',
+                color: '#323F73',
+                cursor: 'pointer',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                margin: showCreateProjects ? '200px 0' : '0',
+              }}
+              size="txtPoppinsBold22"
+              onClick={handleCreateProjectClick}
+            >
+              {showCreateProjects ? 'Create' : 'My Projects'}
+            </Text>
+            {!showCreateProjects && (
+              <div style={{ marginTop: '30px' }}>
+                {projects.map((project) => (
+                  <div
+                    key={project.id}
+                    style={styles.projectItem}
+                    onClick={() => navigate(`/project/${project.id}`)}
+                  >
+                    <Text className="common-pointer" size="txtPoppinsRegular16" color="indigo-800">
+                      {project.name}
+                    </Text>
+                    <Text color="indigo-800">{`Due ${project.dueDate}`}</Text>
                   </div>
-                </div>
+                ))}
               </div>
-            </div>
+            )}
           </div>
 
           <div
@@ -225,34 +143,37 @@ const DashboardPage = () => {
             onMouseLeave={handleTasksLeave}
             className="bg-gray-50 flex flex-1 flex-col items-center justify-center p-[30px] sm:px-5 rounded-[30px] w-full"
           >
-            <div>
-              <div className="flex flex-col items-center justify-center mb-[189px] mt-[26px] w-[99%] md:w-full relative">
-                <div>
-                  <Text
-                    onClick={handleTaskClick}
-                    style={{
-                      fontSize: showCreateButton ? '40px' : '22px',
-                      color: '#323F73',
-                      height: '32px',
-                      width: '165px',
-                      cursor: 'pointer',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      margin: showCreateButton ? '200px 0' : '0',
-                    }}
-                    size="txtPoppinsBold22"
+            <Text
+              style={{
+                fontSize: showCreateTasks ? '40px' : '22px',
+                color: '#323F73',
+                cursor: 'pointer',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                margin: showCreateTasks ? '200px 0' : '0',
+              }}
+              size="txtPoppinsBold22"
+              onClick={handleCreateTaskClick}
+            >
+              {showCreateTasks ? 'Create' : 'My Tasks'}
+            </Text>
+            {!showCreateTasks && (
+              <div style={{ marginTop: '30px' }}>
+                {tasks.map((task) => (
+                  <div
+                    key={task.id}
+                    style={styles.projectItem}
+                    onClick={() => navigate(`/task/${task.id}`)}
                   >
-                    {showCreateButton ? 'Create' : 'My Tasks'}
-                  </Text>
-                  <div className="flex flex-col gap-11 items-start justify-start md:ml-[0] ml-[17px] mt-[78px] w-[84%] md:w-full">
-                    <div className="flex flex-row items-center justify-between w-full">
-                      {renderTasks()}
-                    </div>
+                    <Text className="common-pointer" size="txtPoppinsRegular16" color="indigo-800">
+                      {task.name}
+                    </Text>
+                    <Text color="indigo-800">{`Due ${task.dueDate}`}</Text>
                   </div>
-                </div>
+                ))}
               </div>
-            </div>
+            )}
           </div>
         </List>
       </div>
